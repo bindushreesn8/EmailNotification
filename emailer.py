3a6c9d104413361735c2b00fc5eae65ac4c78594
bindushreesn <123581773+bindushreesn8@users.noreply.github.com>
2023-03-07 15:32:12 +0530

    updated script

--- emailer.py
+++ emailer.py
@@ -7,13 +7,13 @@ s = smtplib.SMTP('smtp.gmail.com', 587)
 s.starttls()
 
 # Authentication
-s.login("dbcpolarbear@gmail.com", "Luckyammulu@123")
+s.login("dbcpolarbear@gmail.com", "sdguyoyuvnnqdyhw")
 
 # message to be sent
-message = "Hello Bindushree"
+message = "Hello Surendra Sir, how you are doing??, Have a good Day"
 
 # sending the mail
-s.sendmail("dbcpolarbear@gmail.com", "sevaopsb@gmail.com", message)
+s.sendmail("dbcpolarbear@gmail.com", "surendra.edcs@gmail.com", message)
 
 # terminating the session
 s.quit()
