4f8e6dc116c8318886e82a3b0ed0567688f2befb
bindushreesn <123581773+bindushreesn8@users.noreply.github.com>
2023-03-09 22:34:44 +0530

    latest update

--- emailer.py
+++ emailer.py
@@ -10,10 +10,10 @@ s.starttls()
 s.login("dbcpolarbear@gmail.com", "sdguyoyuvnnqdyhw")
 
 # message to be sent
-message = "Hello Surendra Sir, how you are doing??, Have a good Day"
+message = "Hello Harsha Sir, how you are doing??, Have a good Sleep"
 
 # sending the mail
-s.sendmail("dbcpolarbear@gmail.com", "sevaopsb@gmail.com", message)
+s.sendmail("dbcpolarbear@gmail.com", "hemanthkumar.edcs@gmail.com", message)
 
 # terminating the session
 s.quit()
